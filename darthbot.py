f6c9211cde61560dc123522bcd06c854fe986f7e
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-29 09:23:12 +1000

    lol

---
+++ darthbot.py
@@ -0,0 +1,18 @@
+import discord as d
+import random
+import requests
+from lxml import html
+client = d.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+client.run('MzYzMDg0ODMxOTMzODU3Nzk0.DK8EnQ.j_zBpWJJGruxHXbbF0EPBW0bYfI')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        await add_reaction(message, ':intel:')
