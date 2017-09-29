5097cbb607dd5644f7605428c390561b53a06eb6
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-29 12:30:34 +1000

    Ughh not working

--- darthbot.py
+++ darthbot.py
@@ -2,6 +2,7 @@ import discord as d
 import random
 import requests
 from lxml import html
+import asyncio
 client = d.Client()
 
 @client.event
@@ -10,10 +11,14 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-client.run('MzYzMDg0ODMxOTMzODU3Nzk0.DK8EnQ.j_zBpWJJGruxHXbbF0EPBW0bYfI')
+
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!test'):
         print("testing")
-        await add_reaction(message, ':military_medal: ')
+        await client.add_reaction(message, '/:military_medal: ')
+
+
+
+client.run('MzYzMDg0ODMxOTMzODU3Nzk0.DK8EnQ.j_zBpWJJGruxHXbbF0EPBW0bYfI')
